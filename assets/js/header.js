80b01ba7725e68534977dd272d6f914e2d7a1e96
Nicolas Declerck <nicolas.declerck@noaxp.com>
2025-10-05 17:44:48 +0200

    Ajout du footer bureau et mobile

--- assets/js/header.js
+++ assets/js/header.js
@@ -34,3 +34,10 @@ fetch("/partials/header.html")
       });
     }
   });
+
+// insert footer.html into footer tags
+fetch("/partials/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.querySelector("footer").innerHTML = data;
+  });
