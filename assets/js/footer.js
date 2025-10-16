77c243edc6f911f6bd4308c926335808cfe77564
Nicolas Declerck <nicolas.declerck@noaxp.com>
2025-10-16 09:10:29 +0200

    Ajustements front

--- assets/js/footer.js
+++
@@ -1,6 +0,0 @@
-// insert footer.html into footer tags
-fetch("/partials/footer.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.querySelector("footer").innerHTML = data;
-  });
